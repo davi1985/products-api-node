b8327345e5c449740c1e2393d4825a0bea81cb46
Davi Silva <davisilvaphoto@gmail.com>
2026-06-05 10:47:08 -0300

    feat(app): integrate Swagger UI at /api-docs endpoint

--- src/factories/app-factory.ts
+++ src/factories/app-factory.ts
@@ -1,12 +1,16 @@
 import express, { type Express } from "express";
+import swaggerUi from "swagger-ui-express";
 import { setupMiddleware } from "../middleware/setup-middleware.js";
 import { createRoutes } from "../routes/index.js";
 import { createServices } from "./service-factory.js";
+import { swaggerSpec } from "../config/swagger-config.js";
 
 export const createApp = (): Express => {
   const app = express();
 
   setupMiddleware(app);
+
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   const { productController, healthController } = createServices();
   app.use(createRoutes(productController, healthController));
